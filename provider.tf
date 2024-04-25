8c67dae7a54269df2e4ef7c59a802b865d2e418e
saikumar95 <rayapudisaikumar@gmail.com>
2024-04-25 19:21:22 +0530

    fivhief

--- provider.tf
+++ provider.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_version = ">=1.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.95.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
-  cloud {
-    organization = "terraform_sai"
-
-    workspaces {
-      name = "terraformCloud_test"
-    
-    }
-  }
-}
 provider "azurerm" {
   # Configuration options
   skip_provider_registration = true
